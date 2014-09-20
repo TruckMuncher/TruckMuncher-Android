590de1cbfef21e79f0825c40a7e6a840ed0afdc6
Brett Nockerts <nockertsb@msoe.edu>
2014-09-20 12:04:48 -0500

    Log in with Facebook PoC. (cherry picked from commit 3155b0a)

--- mobile/src/main/java/com/truckmuncher/truckmuncher/LoginFragment.java
+++ mobile/src/main/java/com/truckmuncher/truckmuncher/LoginFragment.java
@@ -10,6 +10,7 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.androidsocialnetworks.lib.SocialNetworkManager;
+import com.androidsocialnetworks.lib.SocialPerson;
 import com.androidsocialnetworks.lib.listener.OnLoginCompleteListener;
 
 public class LoginFragment extends Fragment
@@ -17,7 +18,11 @@ public class LoginFragment extends Fragment
 
     private SocialNetworkManager socialNetworkManager;
     private ImageButton twitterButton;
+    private ImageButton facebookButton;
     private TextView twitterStatusView;
+    private TextView facebookStatusView;
+    private SocialPerson twitterUser;
+    private SocialPerson facebookUser;
 
     public static final String SOCIAL_NETWORK_TAG = "MainActivity.SOCIAL_NETWORK_TAG";
 
@@ -36,7 +41,9 @@ public class LoginFragment extends Fragment
         if (socialNetworkManager == null) {
             socialNetworkManager = SocialNetworkManager.Builder.from(getActivity())
                     .twitter("KCWZ6nlCEykT9S7AjGqJwEsM8", "userPqxIrLjeKGDrURvZMWclgIWKsP5WslVEadMU7ii1cJtSqF")
+                    .facebook()
                     .build();
+            socialNetworkManager.setOnInitializationCompleteListener(this);
             getFragmentManager().beginTransaction().add(socialNetworkManager, SOCIAL_NETWORK_TAG).commit();
         }
     }
@@ -46,32 +53,35 @@ public class LoginFragment extends Fragment
         super.onViewCreated(view, savedInstanceState);
 
         twitterButton = (ImageButton) view.findViewById(R.id.twitter_button);
+        facebookButton = (ImageButton) view.findViewById(R.id.facebook_button);
         twitterStatusView = (TextView) view.findViewById(R.id.twitter_status);
+        facebookStatusView = (TextView) view.findViewById(R.id.facebook_status);
 
         twitterButton.setOnClickListener(this);
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-
-        if (isLoggedInToTwitter()) {
-            logOutOfTwitter();
-        }
+        facebookButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        if (isLoggedInToTwitter()) {
-            logOutOfTwitter();
-        } else {
-            loginWithTwitter();
+        if (view == twitterButton) {
+            if (isLoggedInToTwitter()) {
+                logOutOfTwitter();
+            } else {
+                loginWithTwitter();
+            }
+        } else if (view == facebookButton) {
+            if (isLoggedInToFacebook()) {
+                logOutOfFacebook();
+            } else {
+                loginWithFacebook();
+            }
         }
     }
 
     @Override
     public void onSocialNetworkManagerInitialized() {
-
+        updateTwitterStatus();
+        updateFacebookStatus();
     }
 
     private void loginWithTwitter() {
@@ -88,16 +98,39 @@ public class LoginFragment extends Fragment
         });
     }
 
+    private void loginWithFacebook() {
+        socialNetworkManager.getFacebookSocialNetwork().requestLogin(new OnLoginCompleteListener() {
+            @Override
+            public void onLoginSuccess(int socialNetworkID) {
+                updateFacebookStatus();
+            }
+
+            @Override
+            public void onError(int socialNetworkID, String requestID, String errorMessage, Object data) {
+                Log.d("Login Fragment", "Facebook login failed: " + errorMessage);
+            }
+        });
+    }
+
     private void logOutOfTwitter() {
         socialNetworkManager.getTwitterSocialNetwork().logout();
         updateTwitterStatus();
+    }
+
+    private void logOutOfFacebook() {
+        socialNetworkManager.getFacebookSocialNetwork().logout();
+        updateFacebookStatus();
     }
 
     private void updateTwitterStatus() {
         String message;
 
         if (isLoggedInToTwitter()) {
-            message = "Signed in";
+            if (twitterUser == null) {
+                message = "Signed in";
+            } else {
+                message = "Signed in as @" + twitterUser.nickname;
+            }
         } else {
             message = "Signed out";
         }
@@ -105,8 +138,28 @@ public class LoginFragment extends Fragment
         twitterStatusView.setText(message);
     }
 
+    private void updateFacebookStatus() {
+        String message;
+
+        if (isLoggedInToFacebook()) {
+            if (facebookUser == null) {
+                message = "Signed in";
+            } else {
+                message = "Signed in as @" + facebookUser.nickname;
+            }
+        } else {
+            message = "Signed out";
+        }
+
+        facebookStatusView.setText(message);
+    }
+
     private boolean isLoggedInToTwitter() {
         return socialNetworkManager.getTwitterSocialNetwork().isConnected();
+    }
+
+    private boolean isLoggedInToFacebook() {
+        return socialNetworkManager.getFacebookSocialNetwork().isConnected();
     }
 
 }
