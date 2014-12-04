494f5c015ae4a65e377731a912b61e309b668c38
Brett Nockerts <nockertsb@msoe.edu>
2014-12-03 20:49:32 -0600

    Added search UI to ActionBar and support for Google Now searching.

--- mobile/src/main/java/com/truckmuncher/truckmuncher/MainActivity.java
+++ mobile/src/main/java/com/truckmuncher/truckmuncher/MainActivity.java
@@ -2,13 +2,19 @@ package com.truckmuncher.truckmuncher;
 
 import android.accounts.Account;
 import android.accounts.AccountManager;
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.SearchView;
 import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.google.android.gms.actions.SearchIntents;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -46,6 +52,8 @@ public class MainActivity extends ActionBarActivity implements GoogleMap.OnInfoW
                 launchVendorMode(accounts[0].name);
             }
         }
+
+        handleIntent(getIntent());
     }
 
     @Override
@@ -57,6 +65,11 @@ public class MainActivity extends ActionBarActivity implements GoogleMap.OnInfoW
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(menu.findItem(R.id.search));
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
         return super.onCreateOptionsMenu(menu);
     }
 
@@ -70,6 +83,30 @@ public class MainActivity extends ActionBarActivity implements GoogleMap.OnInfoW
             return true;
         } else {
             return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+
+        handleIntent(intent);
+    }
+
+    /**
+     * Handles any intents that are passed to this activity by identifying them and taking the
+     * appropriate action. Currently only the {@link Intent#ACTION_SEARCH} and
+     * {@link SearchIntents#ACTION_SEARCH} intents are supported to filter food trucks by the given
+     * search query.
+     * @param intent An intent that was passed to this activity that should be handled.
+     */
+    private void handleIntent(Intent intent) {
+        if (Intent.ACTION_SEARCH.equals(intent.getAction()) ||
+                SearchIntents.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+
+            // TODO: Send search request to API.
+            Toast.makeText(this, "You searched " + query, Toast.LENGTH_LONG).show();
         }
     }
 
