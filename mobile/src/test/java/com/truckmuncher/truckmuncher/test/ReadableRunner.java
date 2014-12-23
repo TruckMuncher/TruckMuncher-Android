23e4721df2cd98310d4cc4d30830c6e242825300
Marius Volkhart <Marius@volkhart.com>
2014-12-22 20:50:57 -0600

    Actually needed a whole bunch more steps to get unit tests up and running (cherry picked from commit 34decba)

--- mobile/src/test/java/com/truckmuncher/truckmuncher/test/ReadableRunner.java
+++ mobile/src/test/java/com/truckmuncher/truckmuncher/test/ReadableRunner.java
@@ -10,21 +10,8 @@ public class ReadableRunner extends BlockJUnit4ClassRunner {
         super(klass);
     }
 
-    public static String humanize(String camelCase) {
-        char[] chars = camelCase.toCharArray();
-        StringBuilder sb = new StringBuilder();
-        sb.append(Character.toUpperCase(chars[0]));
-        for (int i = 1; i < chars.length; i++) {
-            if (Character.isUpperCase(chars[i])) {
-                sb.append(' ');
-            }
-            sb.append(chars[i]);
-        }
-        return sb.toString();
-    }
-
     @Override
     protected String testName(FrameworkMethod method) {
-        return humanize(method.getName());
+        return StringUtils.humanize(method.getName());
     }
 }
