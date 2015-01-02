227947c7978af2232c1f67946208e453b1dfa4ea
Marius Volkhart <Marius@volkhart.com>
2015-01-01 18:54:54 -0600

    Menu View staging

--- mobile/src/main/res/raw/truckmuncher.sql
+++ mobile/src/main/res/raw/truckmuncher.sql
@@ -45,7 +45,7 @@ CREATE VIEW `truck` AS SELECT
     `is_serving`,
     `latitude`,
     `longitude`,
-    `is_dirty`,
+    `is_dirty`
 
     FROM
     `truck_properties`INNER JOIN `truck_state`
@@ -77,3 +77,29 @@ CREATE TABLE `menu_item` (
 
     FOREIGN KEY(`category_id`) REFERENCES `category`(`id`)
 );
+
+CREATE VIEW `menu` AS SELECT
+    `menu_item`.`_id` AS `_id`,
+    `menu_item`.`id` AS `menu_item_id`,
+    `menu_item`.`name` AS `menu_item_name`,
+    `price`,
+    `menu_item`.`notes` AS `menu_item_notes`,
+    `order_in_category`,
+    `is_available`,
+
+    `category`.`name` AS `category_name`,
+    `category`.`id` AS `category_id`,
+    `category`.`notes` AS `category_notes`,
+    order_in_menu,
+
+    `truck_properties`.`id` AS `truck_id`
+
+    FROM
+    `menu_item` INNER JOIN `category`
+    ON `menu_item`.`category_id` = `category`.`id`
+
+    INNER JOIN `truck_properties`
+    ON `category`.`truck_id` = `truck_properties`.`id`
+
+    ORDER BY `order_in_menu`, `order_in_category`
+;
