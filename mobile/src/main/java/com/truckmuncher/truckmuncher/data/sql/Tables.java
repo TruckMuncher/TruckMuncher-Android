c10d7104d57a063e1a354f24898eb0095b7ce13c
Marius Volkhart <Marius@volkhart.com>
2015-01-01 21:46:09 -0600

    Tables has everything listed now too

--- mobile/src/main/java/com/truckmuncher/truckmuncher/data/sql/Tables.java
+++ mobile/src/main/java/com/truckmuncher/truckmuncher/data/sql/Tables.java
@@ -6,4 +6,6 @@ public interface Tables {
     public static final String TRUCK_STATE = "truck_state";
     public static final String CATEGORY = "category";
     public static final String MENU_ITEM = "menu_item";
+    public static final String TRUCK = "truck";
+    public static final String MENU = "menu";
 }
