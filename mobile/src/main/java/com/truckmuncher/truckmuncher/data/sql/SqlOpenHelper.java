15bce1cd783df1ea861c5bf7d733df26fd4d9997
Marius Volkhart <Marius@volkhart.com>
2015-01-01 18:57:47 -0600

    SqlOpenHelper to make use of all the tables we just changes around

--- mobile/src/main/java/com/truckmuncher/truckmuncher/data/sql/SqlOpenHelper.java
+++ mobile/src/main/java/com/truckmuncher/truckmuncher/data/sql/SqlOpenHelper.java
@@ -1,16 +1,30 @@
 package com.truckmuncher.truckmuncher.data.sql;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.annotation.NonNull;
+import android.support.annotation.RawRes;
+
+import com.truckmuncher.truckmuncher.R;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import timber.log.Timber;
 
 public abstract class SqlOpenHelper extends SQLiteOpenHelper {
 
     public static final int VERSION = 1;
 
+    private final Context context;
+
     protected SqlOpenHelper(Context context, String dbName, int version) {
         super(context, dbName, null, version);
+        this.context = context.getApplicationContext();
     }
 
     public static SqlOpenHelper newInstance(Context context) {
@@ -19,22 +33,59 @@ public abstract class SqlOpenHelper extends SQLiteOpenHelper {
 
     @Override
     public final void onCreate(@NonNull SQLiteDatabase db) {
-        TruckTable.onCreate(db);
-        TruckStateTable.onCreate(db);
-        TruckView.onCreate(db);
-        CategoryTable.onCreate(db);
-        MenuItemTable.onCreate(db);
-        MenuView.onCreate(db);
+
+        // Create structure
+        readAndExecuteSQLScript(db, context, R.raw.truckmuncher);
     }
 
     @Override
     public final void onUpgrade(@NonNull SQLiteDatabase db, int oldVersion, int newVersion) {
-        TruckTable.onUpgrade(db, oldVersion, newVersion);
-        TruckStateTable.onUpgrade(db, oldVersion, newVersion);
-        TruckView.onUpgrade(db, oldVersion, newVersion);
-        CategoryTable.onUpgrade(db, oldVersion, newVersion);
-        MenuItemTable.onUpgrade(db, oldVersion, newVersion);
-        MenuView.onUpgrade(db, oldVersion, newVersion);
+        for (int i = oldVersion; i < newVersion; ++i) {
+            String migrationFileName = String.format("from_%d_to_%d", i, (i + 1));
+            Timber.d("Looking for migration file: %s", migrationFileName);
+            int migrationFileResId = context.getResources().getIdentifier(migrationFileName, "raw", context.getPackageName());
+
+            if (migrationFileResId != 0) {
+
+                // execute script
+                readAndExecuteSQLScript(db, context, migrationFileResId);
+
+            } else {
+                Timber.d("Not found: %s", migrationFileName);
+            }
+        }
+    }
+
+    private void readAndExecuteSQLScript(SQLiteDatabase db, Context context, @RawRes int sqlScriptResId) {
+        Resources res = context.getResources();
+
+        try {
+            InputStream is = res.openRawResource(sqlScriptResId);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+
+            executeSQLScript(db, reader);
+
+            reader.close();
+            is.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to read SQL script", e);
+        }
+    }
+
+    private void executeSQLScript(SQLiteDatabase db, BufferedReader reader) throws IOException {
+        String line;
+        StringBuilder statement = new StringBuilder();
+        while ((line = reader.readLine()) != null) {
+            statement.append(line);
+            statement.append("\n");
+            if (line.endsWith(";")) {
+                String toExec = statement.toString();
+                Timber.d("Executing script: %s", toExec);
+                db.execSQL(toExec);
+                statement = new StringBuilder();
+            }
+        }
     }
 
     private static class SqlOpenHelperImpl extends SqlOpenHelper {
