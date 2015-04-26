e789726b756b709b10d65d31963573766cff9e72
Marius Volkhart <Marius@volkhart.com>
2015-04-26 00:26:45 -0500

    Upgrade to the official test support libraries

--- mobile/src/androidTest/java/com/truckmuncher/app/customer/TruckClusterRendererTest.java
+++ mobile/src/androidTest/java/com/truckmuncher/app/customer/TruckClusterRendererTest.java
@@ -1,31 +1,39 @@
 package com.truckmuncher.app.customer;
 
-import android.test.AndroidTestCase;
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.maps.android.clustering.Cluster;
 import com.google.maps.android.clustering.ClusterManager;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-public class TruckClusterRendererTest extends AndroidTestCase {
+@RunWith(AndroidJUnit4.class)
+public class TruckClusterRendererTest {
 
     private TruckClusterRenderer<TruckCluster> clusterRenderer;
 
-    @Override
+    @Before
     public void setUp() throws Exception {
-        super.setUp();
+        Context context = InstrumentationRegistry.getContext();
 
-        GoogleMap map = new MapView(mContext).getMap();
+        GoogleMap map = new MapView(context).getMap();
 
-        ClusterManager<TruckCluster> clusterManager = new ClusterManager<>(mContext, map);
-        clusterRenderer = new TruckClusterRenderer<>(mContext, map, clusterManager);
+        ClusterManager<TruckCluster> clusterManager = new ClusterManager<>(context, map);
+        clusterRenderer = new TruckClusterRenderer<>(context, map, clusterManager);
     }
 
-    public void testShouldRenderAsCluster() {
+    @Test
+    public void shouldRenderAsCluster() {
         Cluster<TruckCluster> cluster = mock(Cluster.class);
 
         when(cluster.getSize())
